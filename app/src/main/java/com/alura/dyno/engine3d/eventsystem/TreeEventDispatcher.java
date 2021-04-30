db05ddcbfb2b5531cbc878fe86f726fdc0c4841b
luislongo <luis.longof@gmail.com>
2021-04-29 22:01:18 -0300

    Create cache structure

--- app/src/main/java/com/alura/dyno/engine3d/eventsystem/TreeEventDispatcher.java
+++ app/src/main/java/com/alura/dyno/engine3d/eventsystem/TreeEventDispatcher.java
@@ -5,22 +5,33 @@ import com.alura.dyno.engine3d.eventsystem.handlers.ITreeEventHandler;
 import com.alura.dyno.engine3d.glyph.Glyph;
 import com.alura.dyno.engine3d.tree.EventTreeIterator;
 
+import java.util.HashMap;
+
 public class TreeEventDispatcher {
     private Glyph root;
+    private HashMap<TreeEventType, EventTreeIterator> cache;
 
     public TreeEventDispatcher(Glyph root) {
         this.root = root;
+        cache = new HashMap<>();
     }
 
     public void sendDownTheTree(IEvent event) {
-        TreeEventType type = event.getType();
-        EventTreeIterator iterator = EventTreeIterator.fromNodeDown(root, type);
+        EventTreeIterator iterator = getEventTreeIterator(event);
 
         while(iterator.hasNext()) {
             ITreeEventHandler handler = iterator.next();
             handler.onExecute(event);
         }
     }
+    private EventTreeIterator getEventTreeIterator(IEvent event) {
+        if(cache.containsKey(event.getType())) {
+            return cache.get(event.getType());
+        } else {
+            return EventTreeIterator.fromNodeDown(root, event.getType());
+        }
+    }
+
     public void sendDownTheChildrenOf(Glyph parent, IEvent event) {
         TreeEventType type = event.getType();
         EventTreeIterator iterator = EventTreeIterator.fromNodeDown(parent, type);
